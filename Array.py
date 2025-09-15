d8473bae4b9ca7f6e89d06c54f084d877c7fa816
Jorimam <iamgodwinjoewin@gmail.com>
2025-09-15 12:44:41 +0100

    first commit

---
+++ Array.py
@@ -0,0 +1,75 @@
+
+class Array:
+    def __init__(self, index = 0, value = None):
+        self.array_length = 0
+        self.item = {}
+        self.index = index
+        self.item[index] = value
+
+        # if index >= self.array_length:
+        #     print("Wrong indexing")
+        # return self.item[index]
+        
+#Append method
+
+    def append(self, item):
+        self.item[self.array_length] = item
+        self.array_length += 1
+        
+        # for i in range(self.array_length):
+        #     # return [self.item[i]]
+        #     print([self.item[i]])
+
+        print([self.item[i] for i in range(self.array_length)])
+
+
+        
+#Insert method
+     
+
+    def insert(self, position, item_to_insert):
+        self.item_to_insert = item_to_insert
+        self.position = position
+
+        self.item[self.position] = self.item_to_insert
+        
+
+#Pop method
+
+    def pop(self, item_to_pop = None):
+       self.item_to_pop = item_to_pop
+       if item_to_pop is None:
+           
+            
+           for i in self.item:
+               pass
+            
+           del self.item[i]
+           self.array_length -= 1
+           print([self.item[i] for i in range(self.array_length)])
+        
+
+       elif self.item_to_pop > self.array_length:
+           print("Wrong Indexing")
+       else:
+           self.item_to_pop = item_to_pop 
+           del self.item[item_to_pop]
+
+
+#Remove method
+        
+    def remove(self, item_to_remove):
+        self.item_to_remove = item_to_remove
+        if item_to_remove in self.item:
+            del item_to_remove
+
+            print([self.item[i] for i in range(self.array_length)])
+            
+    
+
+
+
+
+
+
+
---
+++ main1.py
@@ -0,0 +1,38 @@
+# from array import array
+import array
+
+fruits = []
+print(fruits)
+
+fruits.append("mango")
+print(fruits)
+fruits.append("kiwi")
+fruits.append("orange")
+print(fruits)
+fruits.pop(0)
+fruits.insert(0, "lemon")
+print(fruits)
+
+# fruits.remove("kiwi")
+# print(fruits)
+
+#
+# numbers = []
+
+# print(numbers)
+# numbers.append(30)
+# numbers.append(12)
+# numbers.append(23)
+# numbers.append(0)
+# print(numbers)
+# numbers.pop()
+# numbers.insert(1, 100)
+
+# print(numbers)
+
+# names = []
+# others = [1,2,3,]
+# names.append("bob")
+# names.append("pip")
+# names.append(others)
+# print(names)
